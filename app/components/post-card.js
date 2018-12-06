825843907f2afb331b8824bad09cef685513157b
billmix <designer.bill@gmail.com>
2018-12-06 08:28:06 -0800

    adding post-card component

--- app/components/post-card.js
+++ app/components/post-card.js
@@ -1,4 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+}).reopenClass({
+  positionalParams: ['featured_image', 'route', 'title', 'created_at', 'content']
 });
